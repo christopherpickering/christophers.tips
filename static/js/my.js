46711dc24140769d30ad8a787cb9f13b6dc4be0b
Christopher Pickering <pher122s@gmail.com>
2018-11-14 07:49:36 -0600

    updated commit

--- static/js/my.js
+++ static/js/my.js
@@ -29,4 +29,20 @@ $('body').on('click', '.js-toolbar-action', function(event) {
     $window
         .resize(resize)
         .trigger('resize');
+
+// accordion
+
+$(document).ready(function($) {
+    $('#accordion').find('.accordion-toggle').click(function(){
+
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');
+
+      //Hide the other panels
+      $(".accordion-content").not($(this).next()).slideUp('fast');
+
+    });
+  });
+
+  
 })(jQuery);
