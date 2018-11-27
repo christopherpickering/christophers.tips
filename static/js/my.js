3b6750dc08fc79386db406dc226e5358902b8498
Christopher Pickering <pher122s@gmail.com>
2018-11-27 14:42:36 -0600

    updated commit

--- static/js/my.js
+++ static/js/my.js
@@ -30,11 +30,11 @@
         $("#accordion")
             .find(".accordion-toggle")
             .click(function() {
-                //Expand or collapse this panel
+                /* Expand or collapse this panel */
                 $(this)
                     .next()
                     .slideToggle("fast");
-                //Hide the other panels
+                /* Hide the other panels */
                 $(".accordion-content")
                     .not($(this).next())
                     .slideUp("fast");
