ef8a8b5ba67bfe0ea83a9a33e9ff69b393ee2c35
Christopher Pickering <pher122s@gmail.com>
2018-11-27 14:36:42 -0600

    updated commit

--- static/js/my.js
+++ static/js/my.js
@@ -1,48 +1,46 @@
- $(document).ready(function(e) {
-console.log('started script')
-$('body').on('click', '.js-toolbar-action', function(event) {
-    event.preventDefault();
-    console.log('clicked')
-        var $img = $('.book');
-        if (!$img.hasClass('with-summary')) {
-            $img.addClass('with-summary');
-        } else {
-            $img.removeClass('with-summary');
-        }
+(function($) {
+    $(document).ready(function(e) {
+        console.log("started script");
+        $("body").on("click", ".js-toolbar-action", function(event) {
+            event.preventDefault();
+            console.log("clicked");
+            var $img = $(".book");
+            if (!$img.hasClass("with-summary")) {
+                $img.addClass("with-summary");
+            } else {
+                $img.removeClass("with-summary");
+            }
+        });
     });
 
-});
-
-
-(function($) {
     var $window = $(window),
-        $img = $('.book');
+        $img = $(".book");
 
     function resize() {
         if ($window.width() > 1200) {
-            return $img.addClass('with-summary');
+            return $img.addClass("with-summary");
         }
 
-        $img.removeClass('with-summary');
+        $img.removeClass("with-summary");
     }
 
-    $window
-        .resize(resize)
-        .trigger('resize');
+    $window.resize(resize).trigger("resize");
 
-// accordion
+    // accordion
 
-$(document).ready(function($) {
-    $('#accordion').find('.accordion-toggle').click(function(){
+    $(document).ready(function($) {
+        $("#accordion")
+            .find(".accordion-toggle")
+            .click(function() {
+                //Expand or collapse this panel
+                $(this)
+                    .next()
+                    .slideToggle("fast");
 
-      //Expand or collapse this panel
-      $(this).next().slideToggle('fast');
-
-      //Hide the other panels
-      $(".accordion-content").not($(this).next()).slideUp('fast');
-
+                //Hide the other panels
+                $(".accordion-content")
+                    .not($(this).next())
+                    .slideUp("fast");
+            });
     });
-  });
-
-  
 })(jQuery);
